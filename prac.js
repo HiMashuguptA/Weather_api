dfa853d36cc5f56a19cbae97aef5bb0fe3edc80b
HiMashuguptA <sg707012@gmail.com>
2023-12-02 07:13:23 +0530

    final code

---
+++ prac.js
@@ -0,0 +1,30 @@
+var city = ""
+var cityName = document.getElementById("city")
+var date = document.getElementById("date")
+var celcius = document.getElementById("celcius")
+var condition = document.getElementById("condition")
+var minMaxTemp = document.getElementById("minmaxtemp")
+function getCity(){
+    var cityName = document.getElementById("search").value
+    city = cityName;
+    console.log(city);
+    weatherData()
+}
+async function weatherData(city="deoria"){
+    const currentDate = new Date();
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    };
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d870156a6aa9a2fc158041714781b23f`);
+    let data = await response.json()
+    console.log(data);
+    cityName.innerText = city +"," + data.sys.country
+    celcius.innerText = Math.floor(data.main.temp - 273.15) + "°C"
+    condition.innerHTML = data.weather[0].description
+    minMaxTemp.innerHTML = Math.floor(data.main.temp_max - 273.15) + "°C" + "/" + Math.floor(data.main.temp_min - 273.15) + "°C"
+    date.innerHTML = formattedDate
+}
